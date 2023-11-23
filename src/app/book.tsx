6de5dbb7baf0bc03f80e79e10f4619b0d6c64077
Iulia Moldovan <iulliaa266@gmail.com>
2023-11-23 16:30:05 +0200

    Persist books in reading list and add remove action

--- src/app/book.tsx
+++ src/app/book.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { usePathname } from "next/navigation";
 import { useBooksContext } from "./books-provider";
+import { useCallback, useMemo } from "react";
 
 export type Book = {
   id: string;
@@ -11,30 +11,48 @@ export type Book = {
 
 export function BookItem({ book }: { book: Book }) {
   const { title, author, publisher } = book || {};
-  const pathname = usePathname();
-  const shouldShowButton = !pathname.includes("/reading-list");
 
   return (
     <div className="flex flex-col border p-4 mb-4 rounded shadow-md">
       <h2 className="text-xl font-bold mb-2">{title}</h2>
       <h5 className="text-md mb-2">Author: {author}</h5>
       <p className="text-sm mb-4">Published by: {publisher}</p>
-      {shouldShowButton && <ActionButton book={book} />}
+      <ActionButton book={book} />
     </div>
   );
 }
 
 function ActionButton({ book }: { book: Book }) {
-  const { addToReadingList, isBookInList } = useBooksContext();
-  const isDisabled = isBookInList(book.id);
+  const { addToReadingList, isBookInList, removeFromReadingList } =
+    useBooksContext();
+
+  const isInList = useMemo(
+    () => isBookInList(book.id),
+    [book.id, isBookInList]
+  );
+
+  const buttonText = useMemo(
+    () => (isInList ? "Remove from reading list" : " Add to reading list"),
+    [isInList]
+  );
+
+  const toggleBookInList = useCallback(
+    (book: Book) => {
+      isInList ? removeFromReadingList(book.id) : addToReadingList(book);
+    },
+    [addToReadingList, isInList, removeFromReadingList]
+  );
 
   return (
     <button
-      onClick={() => addToReadingList(book)}
-      className="disabled:opacity-20 bg-yellow-500 text-white p-2 rounded hover:bg-yellow-600 w-80 self-end mr-10"
-      disabled={isDisabled}
+      onClick={() => toggleBookInList(book)}
+      className={`text-white p-2  w-80 self-end mr-10 rounded ${
+        isInList
+          ? "bg-red-400 hover:bg-red-500"
+          : "bg-yellow-500 hover:bg-yellow-600"
+      }`}
     >
-      Add to reading list
+      {buttonText}
     </button>
   );
 }
--- src/app/books-provider.tsx
+++ src/app/books-provider.tsx
@@ -5,6 +5,7 @@ import {
   createContext,
   useCallback,
   useContext,
+  useEffect,
   useState,
 } from "react";
 import { Book } from "./book";
@@ -12,20 +13,42 @@ import { Book } from "./book";
 type BooksContextType = {
   readingList: Book[] | [];
   addToReadingList: (book: Book) => void;
-  searchValue: string;
-  setSearchValue: (v: string) => void;
   isBookInList: (id: string) => boolean;
+  removeFromReadingList: (id: string) => void;
 };
 
 const BooksContext = createContext<BooksContextType>({} as BooksContextType);
 
 function BooksProvider({ children }: PropsWithChildren<{}>) {
-  const [readingList, setReadingList] = useState<Book[]>([]);
-  const [searchValue, setSearchValue] = useState("");
+  const isClient = typeof window !== "undefined";
+
+  const storedReadingList = isClient ? localStorage.getItem("readingList") : "";
+  const initialReadingList = storedReadingList
+    ? JSON.parse(storedReadingList)
+    : [];
+
+  const [readingList, setReadingList] = useState<Book[] | []>(
+    initialReadingList
+  );
+
+  useEffect(() => {
+    localStorage.setItem("readingList", JSON.stringify(readingList));
+  }, [readingList]);
 
   const addToReadingList = useCallback((book: Book) => {
     setReadingList((prev) => [...prev, book]);
   }, []);
+
+  const removeFromReadingList = useCallback(
+    (bookId: string) => {
+      const updatedReadingList = readingList.filter(
+        (book) => book.id !== bookId
+      );
+
+      setReadingList(updatedReadingList);
+    },
+    [readingList]
+  );
 
   const isBookInList = useCallback(
     (bookId: string) => {
@@ -40,9 +63,8 @@ function BooksProvider({ children }: PropsWithChildren<{}>) {
       value={{
         readingList,
         addToReadingList,
-        searchValue,
-        setSearchValue,
         isBookInList,
+        removeFromReadingList,
       }}
     >
       {children}
